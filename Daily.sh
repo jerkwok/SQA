5006a7eedebd5d7f5c4c5eccbe84f815594f045c
jerkwok <jeremy.kwok@uoit.net>
2016-04-12 18:55:09 -0400

    weekly script is theoretically correct. Daily fixes. Input fixes.

--- Daily.sh
+++ Daily.sh
@@ -11,29 +11,28 @@ cd ..
 rm $6
 
 # Run the frontend 5 times
-./Frontend/banksys ./Frontend/CurrentBankAccounts.txt output0.txt < $1
-./Frontend/banksys ./Frontend/CurrentBankAccounts.txt output1.txt < $2
-./Frontend/banksys ./Frontend/CurrentBankAccounts.txt output2.txt < $3
-./Frontend/banksys ./Frontend/CurrentBankAccounts.txt output3.txt < $4
-./Frontend/banksys ./Frontend/CurrentBankAccounts.txt output4.txt < $5
-#./Frontend/banksys ./dailyInput/CurrentBankAccounts.txt output0.txt < ./dailyInput/0$day/input01
+./Frontend/banksys $5 output_daily0.txt < $1
+./Frontend/banksys $5 output_daily1.txt < $2
+./Frontend/banksys $5 output_daily2.txt < $3
+./Frontend/banksys $5 output_daily3.txt < $4
+./Frontend/banksys $5 output_daily4.txt
 
 #Merge the output files
 for i in 0 1 2 3 4 
 do
 	# echo $i
-	filename="output"
+	filename="output_daily"
 	filename=$filename"$i.txt"
 	cat $filename >> $6
 done
 
 # Remove the non merged output files
-# rm output*
+rm output_daily*
 
 cd Backend/
 
 make
 
-java -cp bin backend.BackEnd ../scripttesting/MasterBankAccounts.txt ../$6 ../scripttesting/NewMasterBankAccounts.txt ../scripttesting/CurrentBankAccounts.txt
+java -cp bin backend.BackEnd ../dailyInput/masterBankAccounts ../$6 ../dailyInput/newmasterBankAccounts.txt ../dailyInput/CurrentBankAccounts.txt
 
 cd ..
--- weekly.sh
+++ weekly.sh
@@ -1,5 +1,5 @@
-for day in {1..7}
-  do
-  	echo "Day $day"
-  	./Daily.sh $day
-done
+# ./Daily.sh ./dailyInput/01/input01 ./dailyInput/01/input02 ./dailyInput/01/input03 ./dailyInput/01/input04 ./dailyInput/CurrentBankAccounts.txt mergedoutput0.txt
+./Daily.sh ./dailyInput/02/input01 ./dailyInput/02/input02 ./dailyInput/02/input03 ./dailyInput/02/input04 ./dailyInput/CurrentBankAccounts.txt mergedoutput1.txt
+# ./Daily.sh ./dailyInput/03/input01 ./dailyInput/03/input02 ./dailyInput/03/input03 ./dailyInput/03/input04 ./dailyInput/CurrentBankAccounts.txt mergedoutput2.txt
+# ./Daily.sh ./dailyInput/04/input01 ./dailyInput/04/input02 ./dailyInput/04/input03 ./dailyInput/04/input04 ./dailyInput/CurrentBankAccounts.txt mergedoutput3.txt
+# ./Daily.sh ./dailyInput/05/input01 ./dailyInput/05/input02 ./dailyInput/05/input03 ./dailyInput/05/input04 ./dailyInput/CurrentBankAccounts.txt mergedoutput4.txt
